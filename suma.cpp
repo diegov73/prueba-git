1be51abe0af22fd28d3c61c19328de94e57f4b81
diegov73 <diego.valdes5@mail.udp.cl>
2025-01-29 22:36:57 -0300

    only "suma" has the code

---
+++ suma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    int a, b, c;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    c = a + b;
+    cout << "The sum of the two numbers is: " << c << endl;
+    return 0;
+}
