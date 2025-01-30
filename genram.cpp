6fa190a89751e3b3dfc9e66e48709740a03a87f9
diegov73 <diego.valdes5@mail.udp.cl>
2025-01-30 02:59:32 -0300

    genram has been improved

--- genram.cpp
+++ genram.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int genram(int n)
 {
-    rand()%n+1;
-
+    return rand()%n+1;
 }
-main()
+int main()
 {
     srand(time(NULL));
     int n;
+    cout << "Enter the highest number that can be gerenated ";
+    cin >> n;
     cout << genram(n) <<endl;
 }
